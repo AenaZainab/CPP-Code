6e56a037fb051341a5cfc91c7da6bb8b5e421185
Aena Zainab <aena.zainab@gmail.com>
2025-09-03 21:46:36 +0500

    Update Typecasting Practice.cpp

--- Typecasting Practice.cpp
+++ Typecasting Practice.cpp
@@ -1,18 +1,14 @@
 #include<iostream>
 using namespace std;
-int c=46;
 int main() {
 	int a=46.9;
 	float b=13;
-	cout<<"the value of a is "<<(int)a<<endl;
-	cout<<"the value of b is "<<(float)b<<endl;
+	cout<<"the value of a is "<<(int)a;
+	cout<<"\nthe value of b is "<<(float)b;
 	//#########you can also write in this method###############
-	cout<<"the expression is "<<a+b<<endl;
-	cout<<"the expression is "<<a+(int)b<<endl;
-	cout<<"the expression is "<<a+int(b)<<endl;
-	
-
-
-return 0;	
+	cout<<"the expression is "<<a+b;
+	cout<<"\nthe expression is "<<b+(int)a;
+	cout<<"\nthe expression is "<<b+int(a);
+	return 0;	
 }
 
