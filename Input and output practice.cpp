e4dc2ad2f8fc91800781d50252b77a265ae4cd87
Aena Zainab <aena.zainab@gmail.com>
2025-09-03 21:03:37 +0500

    Update Input and output practice.cpp

--- Input and output practice.cpp
+++ Input and output practice.cpp
@@ -2,11 +2,12 @@
 using namespace std;
 int main () {
 	int num1 , num2;
+	int num3=num1+num2;
 	cout<<"the value of number 1 is";
 	cin>>num1; 
 	cout<<"the value of number 2 is";
 	cin>>num2;
-	cout<<"the sum is"<<num1+num2;
+	cout<<"the sum is"<<num3;
 	return 0;
 	
 }
